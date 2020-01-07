04102b232a5d395d722461ed42aef769a6201a6d
Rohan Nair <r@rohannair.ca>
2020-01-06 20:32:24 -0500

    Add Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    'react/prop-types': 0,
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
 
 interface AppProps {
-  dummy: string;
+  dummy: string
 }
-const Title: FC<AppProps> = ({ dummy }) => <h1>Hello {dummy}!</h1>;
+
+const Title: FC<AppProps> = ({ dummy }) => <h1>Hello {dummy}!</h1>
+
 const App: FC = () => (
   <div>
     <Title dummy="world" />
   </div>
-);
+)
 
-export default App;
+export default App
